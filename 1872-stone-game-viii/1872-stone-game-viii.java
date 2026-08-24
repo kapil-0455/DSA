46d9c4d21829556297c1f9c688c84c632ecb2299
kapil-0455 <kapil0455cu@gmail.com>
2026-08-24 11:15:37 +0530

    Time: 5 ms (42.98%), Space: 86.9 MB (61.7%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,27 +1,27 @@
 class Solution {
-    int n ;
-    Integer dp[];
-    public int solve(int i  , int prefix[]){
-        if( i == n-1) return prefix[n-1];
-
-        if (dp[i] != null) return dp[i];
-        
-        int take = prefix[i] - solve(i+1 , prefix);
-        int notTake = solve(i+1 , prefix);
-
-        return dp[i] = Math.max(take , notTake);
-    }
     public int stoneGameVIII(int[] stones) {
-        n = stones.length;
+        int n = stones.length;
 
-        dp = new Integer[n+1];
-        int prefixSum[] = new int[n];
+        int[] prefixSum = new int[n];
 
         prefixSum[0] = stones[0];
-        for ( int i =1 ;i < n ;i++){
-            prefixSum[i] = prefixSum[i-1] + stones[i]; 
+        for (int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + stones[i];
         }
 
-        return solve(1 , prefixSum);
+        int[] dp = new int[n];
+
+        // Base case
+        dp[n - 1] = prefixSum[n - 1];
+
+        // Bottom-up
+        for (int i = n - 2; i >= 1; i--) {
+            int take = prefixSum[i] - dp[i + 1];
+            int notTake = dp[i + 1];
+
+            dp[i] = Math.max(take, notTake);
+        }
+
+        return dp[1];
     }
 }
